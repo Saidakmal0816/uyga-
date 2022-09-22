d300b8df2093f9a452135d293bf469a64fc6ce83
Saidakmal0816 <saidakmal468@gmail.com>
2022-09-22 20:02:43 +0500

    uyga_vazifa

---
+++ main.js
@@ -0,0 +1,74 @@
+
+//6
+// let kilo = prompt("1kg kartoshka=5.000so'm.")
+// let kartoshka = 5.000
+// let jami = kilo*kartoshka
+
+
+// console.log(jami+".000so'm");
+
+
+
+
+// // //8
+// let a=5
+// let b=5
+// let sum = 0
+// for (let i = a; i <= b; i++){
+    
+//     sum = sum + i
+// }
+// console.log(sum);
+
+
+
+
+
+
+// //9
+// let a=1
+// let b=5
+// let sum = 0
+// for (let i = a; i <= b; i++){
+//     sum = sum + i*i
+// }
+// console.log(sum);
+
+
+
+
+
+// //12
+// let n=prompt("son yoz juftini yozaman")
+// for (let i = 0; i <=n; i++) {
+//     i+2
+//     console.log(i++);
+// }   
+    
+
+
+
+
+
+
+
+
+
+// // //15
+// let a=prompt("son kirit kamayib borishini yozaman")
+// let b=0
+// for (let i = a; i >= b; i--){
+//  console.log(i);
+// }
+
+
+
+
+// //17
+// let a=1
+// let b=5
+// let sum = 1
+// for (let i = a; i <= b; i++){
+//     sum = sum * i
+// }
+// console.log(sum);
